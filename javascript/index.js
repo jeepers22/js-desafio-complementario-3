e56745ccc7c58c9db16624a1f036660eab0c75dc
Maximiliano Safran <maxizero@gmail.com>
2022-09-19 03:11:35 -0300

    Agregados usando DOM

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,24 @@
 // ARRAYS DE OBJETOS (Globales)
-let usuarios = []
 
+let usuarios = []
 let productos = []
 
+// VAR DOM ELEMENTS
+
+let domLoginTitle
+let domRegistroTitle
+let domLoginForm
+let domLoginUser
+let domLoginPass
+let domLoginBtn
+let domRegistroForm
+let domRegistroUser
+let domRegistroPass
+let domRegistroBtn
+let domProductos
+
 /* ================ CLASE USUARIOS ================ */
+
 class Usuario {
 
     // ATRIBUTOS
@@ -21,16 +36,16 @@ class Usuario {
     }
 }
 
-
 /* ================ CLASE PRODUCTO ================ */
 class Producto {
 
     // ATRIBUTOS
-    constructor(tipoProd, marca, precio, stock) {
+    constructor(tipoProd, marca, precio, stock, imagen) {
         this.tipoProd = tipoProd
         this.marca = marca
         this.precio = precio
         this.stock = stock
+        this.imagen = imagen
     }
 
     // MÉTODOS
@@ -57,8 +72,8 @@ class Producto {
     }
 }
 
-
 /* ================ DECLARACIÓN FUNCIONES ================ */
+
 validarLogin = (userLogin, passLogin) => usuarios.some((usuario) => (usuario.user === userLogin && usuario.password === passLogin))
 
 usuarioExistente = (userAlta) => usuarios.some((usuario) => usuario.user === userAlta)
@@ -159,66 +174,138 @@ function desplegarProcedimientoAdmin(userLogin) {
 }
 
 function desplegarProcedimientoCompra(userLogin) {
+    domProductos.innerHTML = ""  // Evita carga repetida de catálogo ante más de un despliegue de de compra
+    productos.forEach((producto) => domProductos.innerHTML += `
+    <div class="producto-card">
+        <img src="${producto.imagen}" alt="textoPrueba" class="producto-img">
+        <div class= "producto__info">
+            <h3>Producto: ${producto.tipoProd} - ${producto.marca}</h3>
+            <p>Precio: ${producto.precio} - Cantidad: ${producto.stock}</p>
+            <button id="agregar-carrito" class="agregar-carrito">Agregar al carrito</button>
+        </div>
+    </div>
+    `)
 
-    let eleccionMenuCompra = 0
-    let eleccionMenuRepeat = 0
-    let salirMenuCompra = 0
-    let cantDeseadaProd = 0
-    let prodElegido = {}
+    console.log(domProductos.innerHTML)
+}
 
-    while (true) { // itera hasta que el usuario salga o deje de comprar productos
+// function desplegarProcedimientoCompra(userLogin) {
 
-        salirMenuCompra = productos.length + 1
+//     let eleccionMenuCompra = 0
+//     let eleccionMenuRepeat = 0
+//     let salirMenuCompra = 0
+//     let cantDeseadaProd = 0
+//     let prodElegido = {}
 
-        while (true) { // itera hasta que la opción ingresada sea válida
-            eleccionMenuCompra = showMenu("menuCompra", userLogin)
-            if (eleccionMenuCompra > 0 && eleccionMenuCompra <= salirMenuCompra) {
-                break
-            }
-            alert("Opción incorrecta")
-        }
+//     while (true) { // itera hasta que el usuario salga o deje de comprar productos
 
-        if (eleccionMenuCompra === salirMenuCompra) {
-            break
-        }
-        else {
-            prodElegido = productos[eleccionMenuCompra - 1]
+//         salirMenuCompra = productos.length + 1
 
-            while (true) { // itera hasta que la opción ingresada sea válida
-                cantDeseadaProd = parseInt(prompt(`Especifique la cantidad de unidades de ${prodElegido.tipoProd} ${prodElegido.marca} que desea comprar`))
-                if (cantDeseadaProd > 0) {
-                    break
-                }
-                alert("Opción incorrecta")
-            }
+//         while (true) { // itera hasta que la opción ingresada sea válida
+//             eleccionMenuCompra = showMenu("menuCompra", userLogin)
+//             if (eleccionMenuCompra > 0 && eleccionMenuCompra <= salirMenuCompra) {
+//                 break
+//             }
+//             alert("Opción incorrecta")
+//         }
 
-            prodElegido.comprar(cantDeseadaProd)
+//         if (eleccionMenuCompra === salirMenuCompra) {
+//             break
+//         }
+//         else {
+//             prodElegido = productos[eleccionMenuCompra - 1]
 
-            while (true) { // itera hasta que la opción ingresada sea válida
-                eleccionMenuRepeat = parseInt(prompt("Desea realizar otra compra?\n1.  SI\n2.  NO"))
-                if ((eleccionMenuRepeat === 1 || eleccionMenuRepeat === 2)) {
-                    break
-                }
-                alert("Opción incorrecta")
-            }
+//             while (true) { // itera hasta que la opción ingresada sea válida
+//                 cantDeseadaProd = parseInt(prompt(`Especifique la cantidad de unidades de ${prodElegido.tipoProd} ${prodElegido.marca} que desea comprar`))
+//                 if (cantDeseadaProd > 0) {
+//                     break
+//                 }
+//                 alert("Opción incorrecta")
+//             }
 
-            if (eleccionMenuRepeat === 2) {
-                break
-            }
-        }
+//             prodElegido.comprar(cantDeseadaProd)
+
+//             while (true) { // itera hasta que la opción ingresada sea válida
+//                 eleccionMenuRepeat = parseInt(prompt("Desea realizar otra compra?\n1.  SI\n2.  NO"))
+//                 if ((eleccionMenuRepeat === 1 || eleccionMenuRepeat === 2)) {
+//                     break
+//                 }
+//                 alert("Opción incorrecta")
+//             }
+
+//             if (eleccionMenuRepeat === 2) {
+//                 break
+//             }
+//         }
+//     }
+// }
+
+/* ================ ELEMENTOS DEL DOM ================ */
+
+function domElementsInit() {
+    domLoginTitle = document.getElementById("login-titulo")
+    domRegistroTitle = document.getElementById("registro-titulo")
+    domLoginForm = document.getElementById("login-form")
+    domLoginUser = document.getElementById("login-user")
+    domLoginPass = document.getElementById("login-pass")
+    domLoginBtn = document.getElementById("login-btn")
+    domRegistroForm = document.getElementById("registro-form")
+    domRegistroUser = document.getElementById("registro-user")
+    domRegistroPass = document.getElementById("registro-pass")
+    domRegistroLoginBtn = document.getElementById("registro-btn")
+    domProductos = document.getElementById("productos-container")
+}
+
+/* ================ EVENTOS DEL DOM ================ */
+
+function obtenerLoginPorDOM() {
+    domLoginForm?.addEventListener("submit", gestionarLogin) // Al cambiar de HTML hay que verificar si el evento existe, sino da error
+}
+
+function obtenerRegistroPorDOM() {
+    domRegistroForm?.addEventListener("submit", gestionarAlta) // Al cambiar de HTML hay que verificar si el evento existe, sino da error
+}
+
+function gestionarLogin(event) {
+    event.preventDefault()
+    let objectUser = new Usuario(domLoginUser.value, domLoginPass.value, false)
+    domLoginForm.reset();
+    domLoginTitle.innerHTML = ""
+    domLoginTitle.innerHTML += `Bienvenido ${objectUser.user}!`
+    if (validarLogin(objectUser.user, objectUser.password)) {
+        !objectUser.esAdmin() ? desplegarProcedimientoCompra(objectUser.user) : desplegarProcedimientoAdmin(objectUser.user)
+    }
+    else {
+        alert("Login fallido - Usuario o contraseña incorrectos")
+    }
+}
+
+function gestionarAlta(event) {
+    event.preventDefault()
+    domRegistroTitle.innerHTML = ""
+    let objectUser = new Usuario(domRegistroUser.value, domRegistroPass.value, false)
+    domRegistroForm.reset();
+    if (!usuarioExistente(objectUser.user)) {
+        objectUser.concretarAltaUsuario()
+        domRegistroTitle.innerHTML += `El usuario ${objectUser.user} se ha registrado exitosamente!`
+        console.log(usuarios)
+    }
+    else {
+        domRegistroTitle.innerHTML += `Alta fallida - El usuario que intenta registrar ya existe`
     }
 }
 
 /* ================ DECLARACIÓN FUNCIÓN PRINCIPAL ================ */
+
 function main() {
 
     let objectUser = {}
 
     // CARGA DE CATÁLOGO
-    productos.push(new Producto("lapicera", "bic", 50, 10))
-    productos.push(new Producto("lapicera", "pelikan", 70, 5))
-    productos.push(new Producto("goma de borrar", "pelikan", 15, 15))
-    productos.push(new Producto("sacapuntas", "maped", 25, 4))
+    productos.push(new Producto("lapicera", "bic", 50, 10, "./img/juv-shora2-300.jpg"))
+    productos.push(new Producto("lapicera", "pelikan", 70, 5, "./img/juv-shora3-300.jpg"))
+    productos.push(new Producto("goma de borrar", "pelikan", 15, 15, "./img/juv-shora3-300.jpg"))
+    productos.push(new Producto("sacapuntas", "maped", 25, 4, "./img/juv-shora4-300.jpg"))
 
     // GENERACIÓN DE USUARIOS
     usuarios.push(new Usuario("admin", "1234", true))
@@ -271,4 +358,17 @@ function main() {
 }
 
 /* ================ LLAMADO FUNCIÓN PRINCIPAL ================ */
-main()
+// main()
+// CARGA DE CATÁLOGO
+productos.push(new Producto("Paleta", "BlackCrown", 60000, 10, "./img/paleta-black.png"))
+productos.push(new Producto("Paleta", "ML10", 50000, 5, "./img/paleta-ml10.png"))
+productos.push(new Producto("Paleta", "Siux", 65000, 15, "./img/paleta-siux.png"))
+productos.push(new Producto("Paleta", "WingPro", 35000, 4, "./img/paleta-wing.png"))
+
+// GENERACIÓN DE USUARIOS
+usuarios.push(new Usuario("admin", "1234", true))
+usuarios.push(new Usuario("maxi", "maxizero", false))
+
+domElementsInit()
+obtenerLoginPorDOM()
+obtenerRegistroPorDOM()
